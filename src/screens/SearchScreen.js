21fc4020f6b136855ffd72c113f7948df02604c9
Alex1.ai <alexanderemmanuel1719@gmail.com>
2023-12-02 18:49:22 +0000

    my first commit for react-native food app

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet} from 'react-native';
+import SearchBar from "../components/SearchBar";
+import yelp from '../api/yelp'
+
+
+const SearchScreen = ()=>{
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const searchApi = async (searchTerm) =>{
+        try{
+
+        
+            const response = await yelp.get('/search',{
+                params:{
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses)
+        }catch(err){
+            console.log(err)
+            setErrorMessage('Something went wrong. Try again')
+
+        }
+    }
+    return (
+        <View style={styles.backgroundStyle}>
+            <SearchBar 
+            term={term} 
+            onTermChange={newTerm=>setTerm(newTerm)}
+            onTermSubmit={() =>searchApi(term)}
+            />
+            {errorMessage ? <Text > {errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle:{
+        backgroundColor: 'white'
+    }
+})
+export default SearchScreen
